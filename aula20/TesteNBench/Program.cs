e09c22cee6b62a1c1fc73dddc357ec40b5d7a523
José Simão <jsimao@cc.isel.ipl.pt>
2018-05-21 10:02:26 +0100

    aula 20

---
+++ aula20/TesteNBench/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBench
+{
+    delegate int MyDelegate(string s, object o);
+
+    class Program
+    {
+        public static int foo()
+        {
+            int r = 2;
+            for (int i = 0; i < 100; ++i)
+                r *= i;
+            return r;
+        }
+
+        public static String bar()
+        {
+            String a = "ave ";
+            for (int i = 0; i < 100; ++i)
+                a += i;
+            return a;
+        }
+
+        public static void TestFoo()
+        {
+            foo();
+        }
+
+        public static void TestBar()
+        {
+            bar();
+        }
+
+        public void InstanceMethod()
+        {
+
+        }
+
+        public static void Main()
+        {
+            /*AVE1718v.NBench.Bench(new Action(TestFoo), "foo");
+            AVE1718v.NBench.Bench(TestFoo, "foo");
+
+            AVE1718v.NBench.Bench(
+                () => foo(), 
+                "foo");
+
+            AVE1718v.NBench.Bench(new Action(TestBar), "bar");
+            */
+
+            Action a1 = () => Console.WriteLine("a1");
+            Action a2 = () => Console.WriteLine("a2");
+            Action a3 = () => Console.WriteLine("a3");
+
+            Action all = a1 + a2;
+            all += a3;
+
+            Program p = new Program();
+            all += new Action(p.InstanceMethod);
+
+            int v = 10;
+
+            Action print = () => Console.WriteLine(v);
+
+            v = 20;
+
+            print();
+        }
+    }
+}
---
+++ aula20/Variance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variance
+{
+    class A { }
+
+    class B : A{ }
+
+    class C : B { }
+
+    delegate void DoSomething(C a);
+
+    delegate A DoSomethingRet(C a);
+
+    class Program
+    {
+        static void M(C a) { }
+
+        static void W(A a) { }
+
+        static B T(C c) { return new B(); }
+
+        static void Main(string[] args)
+        {
+            DoSomething d1 = new DoSomething(W);
+
+            d1(new C());
+
+            DoSomethingRet d2 = new DoSomethingRet(T);
+
+            A a = d2(new C());
+        }
+    }
+}
